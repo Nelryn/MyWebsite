c2a5e310a1dc6553a8afe9f959b3fba26073c424
Jojolions <emilie.baunifais@epitech.eu>
2024-09-08 22:58:31 +0400

    just trying to integrate tailwind css to my project and do a nav bar

---
+++ my-website/src/App.js
@@ -0,0 +1,23 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    // <PageLayout>
+    //   <NavigationHeader>
+    //     <SearchBar />
+    //     <Link to="/docs">Docs</Link>
+    //   </NavigationHeader>
+    //   <Sidebar />
+    //   <PageContent>
+    //     <TableOfContents />
+    //     <DocumentationText />
+    //   </PageContent>
+    // </PageLayout>
+  )
+}
+
+export default App;
