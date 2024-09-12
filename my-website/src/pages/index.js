aac2b251a0d80b274b57cb084c0b8d1d9f190567
Jojolions <emilie.baunifais@epitech.eu>
2024-09-13 02:01:41 +0400

    presentation part wip, stopped at social icons

--- my-website/src/pages/index.js
+++ my-website/src/pages/index.js
@@ -1,16 +1,47 @@
 import React from "react";
+import { VscGithubInverted } from "react-icons/vsc";
+import { SiLinkedin } from "react-icons/si";
 
-import "./index.css"
+import "./index.css";
+import pfp from "../assets/jojolions.png";
 
 const Home = () => {
     return (
-        <body className="bg-baby-blue">
+        <div className="bg-baby-blue">
+            <div className="py-5 flex place-content-center">
+                <div className="bg-[#AAB9CF] w-1/2 h-fit rounded-md shadow-basic py-10 grid gap-3 justify-items-center">
+                    <div className="flex">
+                        <img src={pfp} alt="Emilie's profile picture" className="h-20 w-20 bg-white rounded-full" />
+                        <div className="px-5 content-center">
+                            <div className="font-sans sans-serif text-xl">
+                                Emilie Baunifais
+                            </div>
+                            <div className="text-center font-sans sans-serif text-base text-txt-gray">
+                                IT Student
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-4/6 py-2 text-center font-sans sans-serif text-lg text-white leading-7">
+                        Hi ! I am Emilie, a last year student at Epitech. Nice to meet you here on my website !
+                        I like programming and trying out new things. I'm curious, dedicated and organized and these helped me to stay motivated throughout the years.
+                        Enjoy your stay here !~
+                    </div>
+                    <div className="flex">
+                        <div className="flex bg-white w-7 h-7 place-content-center place-items-center rounded-md">
+                            <VscGithubInverted className="w-6 h-6"/>
+                        </div>
+                        <div>
+                            <SiLinkedin />
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div>
                 <h1 className="text-3xl font-bold underline">
                     Welcome to my Website !
                 </h1>
             </div>
-        </body>
+        </div>
     )
 }
 
--- my-website/tailwind.config.js
+++ my-website/tailwind.config.js
@@ -8,6 +8,10 @@ module.exports = {
     extend: {
       colors: {
         'baby-blue': '#BDD4E7',
+        'txt-gray': '#696A75',
+      },
+      boxShadow: {
+        'basic': '0 4px 4px 0px rgba(0,0,0,0.3)',
       }
     },
   },
